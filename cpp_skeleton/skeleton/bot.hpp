d36a3ccfdda22f8a691c317b615937067ce4d198
david-amirault <david-amirault@users.noreply.github.com>
2019-12-22 05:59:50 +0000

    compiles

--- cpp_skeleton/skeleton/bot.hpp
+++ cpp_skeleton/skeleton/bot.hpp
@@ -17,7 +17,7 @@ class Bot
          * @param round_state Pointer to the RoundState object.
          * @param active Your player's index.
          */
-        virtual void handle_new_round(GameState* game_state, RoundState* round_state, int active);
+        virtual void handle_new_round(GameState* game_state, RoundState* round_state, int active) = 0;
 
         /**
          * Called when a round ends. Called NUM_ROUNDS times.
@@ -26,7 +26,7 @@ class Bot
          * @param terminal_state Pointer to the TerminalState object.
          * @param active Your player's index.
          */
-        virtual void handle_round_over(GameState* game_state, TerminalState* terminal_state, int active);
+        virtual void handle_round_over(GameState* game_state, TerminalState* terminal_state, int active) = 0;
 
         /**
          * Where the magic happens - your code should implement this function.
@@ -37,7 +37,7 @@ class Bot
          * @param active Your player's index.
          * @return Your action.
          */
-        virtual Action get_action(GameState* game_state, RoundState* round_state, int active);
+        virtual Action get_action(GameState* game_state, RoundState* round_state, int active) = 0;
 };
 
 
